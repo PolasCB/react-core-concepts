fed4530852d32f1cf8a6867e46b5380ca2d10157
Polas Chandra Barmon <polaschandrabarmon@gmail.com>
2024-03-01 20:56:13 +0600

    Just save to git

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const personName = ['Polas', 'Ajay', 'Rashed', 'Dipok'];
+  const job = ['Student', 'NTARCA Teacher', 'Businessman', 'Jobless'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Change go to google
-        </a>
+        <p>I am a react person</p>
+        <Person></Person>
+        
+
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  return (
+    <div>
+      <h3>Name: {props.name}</h3>
+      <h5>Profession: {props.job}</h5>
+    </div>
+  )
 }
 
 export default App;
